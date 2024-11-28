b2ee61370cee4bab7e0cac0744d1aadd25c76b30
Nima Naderi <nima.naderi04@gmail.com>
2024-11-28 19:12:24 +0100

    feat: add a new implemenation for Mo's algorithm

--- content/data-structures/MoQueries.h
+++ content/data-structures/MoQueries.h
@@ -1,62 +1,35 @@
 /**
- * Author: Simon Lindholm
- * Date: 2019-12-28
- * License: CC0
- * Source: https://github.com/hoke-t/tamu-kactl/blob/master/content/data-structures/MoQueries.h
+ * Author: Nima Naderi Ghotbodini
+ * Date: 2024-11-28
+ * Source: https://codeforces.com/contest/1514/submission/113524329
  * Description: Answer interval or tree path queries by finding an approximate TSP through the queries,
  * and moving from one query to the next by adding/removing points at the ends.
  * If values are on tree edges, change \texttt{step} to add/remove the edge $(a, c)$ and remove the initial \texttt{add} call (but keep \texttt{in}).
  * Time: O(N \sqrt Q)
- * Status: stress-tested
+ * Status: Tested on Codeforces
  */
 #pragma once
 
-void add(int ind, int end) { ... } // add a[ind] (end = 0 or 1)
-void del(int ind, int end) { ... } // remove a[ind]
-int calc() { ... } // compute current answer
-
-vi mo(vector<pii> Q) {
-	int L = 0, R = 0, blk = 350; // ~N/sqrt(Q)
-	vi s(sz(Q)), res = s;
-#define K(x) pii(x.first/blk, x.second ^ -(x.first/blk & 1))
-	iota(all(s), 0);
-	sort(all(s), [&](int s, int t){ return K(Q[s]) < K(Q[t]); });
-	for (int qi : s) {
-		pii q = Q[qi];
-		while (L > q.first) add(--L, 0);
-		while (R < q.second) add(R++, 1);
-		while (L < q.first) del(L++, 0);
-		while (R > q.second) del(--R, 1);
-		res[qi] = calc();
-	}
-	return res;
+bool CMP(int x, int y){
+    if(Ql[x] / SQR == Ql[y] / SQR) return (Qr[x] < Qr[y]);
+    return (Ql[x] / SQR < Ql[y] / SQR);
 }
-
-vi moTree(vector<array<int, 2>> Q, vector<vi>& ed, int root=0){
-	int N = sz(ed), pos[2] = {}, blk = 350; // ~N/sqrt(Q)
-	vi s(sz(Q)), res = s, I(N), L(N), R(N), in(N), par(N);
-	add(0, 0), in[0] = 1;
-	auto dfs = [&](int x, int p, int dep, auto& f) -> void {
-		par[x] = p;
-		L[x] = N;
-		if (dep) I[x] = N++;
-		for (int y : ed[x]) if (y != p) f(y, x, !dep, f);
-		if (!dep) I[x] = N++;
-		R[x] = N;
-	};
-	dfs(root, -1, 0, dfs);
-#define K(x) pii(I[x[0]] / blk, I[x[1]] ^ -(I[x[0]] / blk & 1))
-	iota(all(s), 0);
-	sort(all(s), [&](int s, int t){ return K(Q[s]) < K(Q[t]); });
-	for (int qi : s) rep(end,0,2) {
-		int &a = pos[end], b = Q[qi][end], i = 0;
-#define step(c) { if (in[c]) { del(a, end); in[a] = 0; } \
-                  else { add(c, end); in[c] = 1; } a = c; }
-		while (!(L[b] <= L[a] && R[a] <= R[b]))
-			I[i++] = b, b = par[b];
-		while (a != b) step(par[a]);
-		while (i--) step(I[i]);
-		if (end) res[qi] = calc();
-	}
-	return res;
+bool CmP(int x, int y){
+    if(Ql[x] / SQR == Ql[y] / SQR) return ((Ql[x] / SQR) % 2) ^ (Qr[x] < Qr[y]);
+    return (Ql[x] / SQR < Ql[y] / SQR);
+}
+void Add(ll x){ x = A[x]; /* ... */ }
+void Ers(ll x){ x = A[x]; /* ... */ }
+int main(){
+    for(int i = 1; i <= q; i ++) cin >> Ql[i] >> Qr[i], vec.push_back(i);
+    sort(vec.begin(), vec.end(), CmP);
+    ll Ml = 1, Mr = 0;
+    for(auto id : vec){
+        int l = Ql[id], r = Qr[id];
+        while(Mr < r) Mr ++, Add(Mr);
+        while(l < Ml) Ml --, Add(Ml);
+        while(Mr > r) Ers(Mr), Mr --;
+        while(Ml < l) Ers(Ml), Ml ++;
+    }
+    return 0;
 }
