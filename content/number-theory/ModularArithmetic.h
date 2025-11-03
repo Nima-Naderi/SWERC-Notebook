e993d30967b3d5b393fae6ec9ef2e64ed3fd2d07
kyooz <165193025+kyoots@users.noreply.github.com>
2025-11-03 21:25:49 +0100

    Update ModularArithmetic.h
    made easier to use/copy, initializes to 0 as default

--- content/number-theory/ModularArithmetic.h
+++ content/number-theory/ModularArithmetic.h
@@ -8,23 +8,25 @@
  */
 #pragma once
 
-#include "euclid.h"
-
-const ll mod = 17; // change to something else
-struct Mod {
+const ll mod = 998244353;
+struct mint {
 	ll x;
-	Mod(ll xx) : x(xx) {}
-	Mod operator+(Mod b) { return Mod((x + b.x) % mod); }
-	Mod operator-(Mod b) { return Mod((x - b.x + mod) % mod); }
-	Mod operator*(Mod b) { return Mod((x * b.x) % mod); }
-	Mod operator/(Mod b) { return *this * invert(b); }
-	Mod invert(Mod a) {
-		ll x, y, g = euclid(a.x, mod, x, y);
-		assert(g == 1); return Mod((x + mod) % mod);
+    mint() : x(0) {}
+	mint(ll xx) : x(xx) {}
+	mint operator+(mint b) { return mint((x + b.x) % mod); }
+	mint operator-(mint b) { return mint((x - b.x + mod) % mod); }
+	mint operator*(mint b) { return mint((x * b.x) % mod); }
+	mint operator/(mint b) { return *this * invert(b); }
+    // mint operator+=(mint b) { return *this = *this + b; } // can be ignored if not necessary
+    // mint operator-=(mint b) { return *this = *this - b; }
+    // mint operator*=(mint b) { return *this = *this * b; }
+    // mint operator/=(mint b) { return *this = *this / b; }
+	mint invert(mint a) { // euclid for non prime mod
+		return a ^ (mod - 2);
 	}
-	Mod operator^(ll e) {
-		if (!e) return Mod(1);
-		Mod r = *this ^ (e / 2); r = r * r;
+	mint operator^(ll e) {
+		if (!e) return mint(1);
+		mint r = *this ^ (e / 2); r = r * r;
 		return e&1 ? *this * r : r;
 	}
 };
