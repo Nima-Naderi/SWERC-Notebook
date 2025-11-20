672e3ce476f1c953cad1a3daab06205af3319610
kyooz <165193025+kyoots@users.noreply.github.com>
2025-11-20 02:30:36 +0100

    added polyarithmetic for FPS operations (#6)

--- content/numerical/PolyArithmetic.h
+++ content/numerical/PolyArithmetic.h
@@ -1,14 +1,22 @@
-// tested on yosupo inv of formal power series
-
-#pragma once
-#include "NumberTheoreticTransform.h"
+/**
+ * Author: kyooz
+ * Date: 2025-11-19
+ * Source: jiangly submissions
+ * Inputs must be in [0, mod).
+ * Time: O(N \log N) for more complex operations (high constant), O(N) for simple operations
+ * Status: tested on yosupo inv, log, exp, pow
+ */
 
 using poly = vector<Z>;
 
-void fix(poly& a) {
-    while (a.size() and a.back().x == 0) {
-        a.pop_back();
-    }
+void fix(poly &a) {
+    while (a.size() and a.back().x == 0) a.pop_back();
+}
+
+poly trunc(poly a, int k) {
+    poly f = a;
+    f.resize(k);
+    return f;
 }
 
 poly add(poly a, poly b) {
@@ -32,20 +40,36 @@ poly mult(poly a, poly b) {
     return conv(a, b); 
 }
 
-poly inv(poly& a, int m) {
-    poly p(1), two(1);
-    two[0] = 2;
-    p[0] = Z(1) / a[0];
-         
-    int k = 1;
-    while (k < m) {
-        k *= 2;
-        poly q(k);
-        for (int i = 0; i < min((int)a.size(), k); i++) q[i] = a[i];
-        p = mult(p, sub(two, mult(q,p)));
-        //p = (p * (two - q * p));
-        p.resize(k);
-    }
-    p.resize(m);
-    return p;
+poly deriv(poly a) {
+    if (a.empty()) return a;
+    poly res(a.size() - 1);
+    for (int i = 0; i < a.size() - 1; ++i) res[i] = a[i + 1] * (i+1);
+    return res;
 }
+
+poly integr(poly a) {
+    poly res(a.size() + 1);
+    for (int i = 0; i < a.size(); ++i) res[i + 1] = a[i] / (i + 1);
+    return res;
+}
+
+poly inv(poly a, int m) {
+    poly x{Z(1)/a[0]};
+    int k = 1;
+    while (k < m) k *= 2, x = trunc(mult(x, (sub(poly{2}, mult(trunc(a, k), x)))), k);
+    return trunc(x, m);
+}
+
+poly log(poly a, int m) {
+    poly res = mult(deriv(a), inv(a, m));
+    res = trunc(integr(res), m);
+    return res;
+}
+    
+poly exp(poly a, int m) {
+    poly x{1};
+    int k = 1;
+    while (k < m) k *= 2, x = trunc(mult(x, add(sub(poly{1}, log(x, k)), trunc(a, k))), k);
+    return trunc(x, m);
+}
+// for pow shift until first non zero coeff, then reshift back, p^x = exp(x * log p) 
