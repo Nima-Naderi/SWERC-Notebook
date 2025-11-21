43573c66452191e52b5ff5ca0b87d43757351f5e
Nima Naderi <nima.naderi04@gmail.com>
2025-11-21 13:07:38 +0100

    fix: change the main template

--- content/contest/template.cpp
+++ content/contest/template.cpp
@@ -5,12 +5,16 @@ using namespace std;
 #define rep(i, a, b) for(int i = a; i < (b); ++i)
 #define all(x) begin(x), end(x)
 #define sz(x) (int)(x).size()
-#define Mp make_pair
+#define MSB(x) (63 - __builtin_clzll(ll(x)))
+#define bit(x, b) ((x >> b) & 1)
 typedef long long ll;
 typedef pair<int, int> pii;
 typedef vector<int> vi;
+template<typename T>
+using vec = vector<T>
 
 int32_t main() {
 	ios::sync_with_stdio(0);cin.tie(0); cout.tie(0);
 	cin.exceptions(cin.failbit);
 }
+//bs._Find_first() ._Find_next(i) .set() .reset() .count()
