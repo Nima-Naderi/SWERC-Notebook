548394cbba0691ca1141f51031eb03888294b96a
Nima Naderi <nima.naderi04@gmail.com>
2025-11-21 14:01:11 +0100

    dump: dump some theories

--- content/data-structures/UnionFindRollback.h
+++ content/data-structures/UnionFindRollback.h
@@ -11,24 +11,20 @@
  */
 #pragma once
 
-struct RollbackUF {
-	vi e; vector<pii> st;
-	RollbackUF(int n) : e(n, -1) {}
-	int size(int x) { return -e[find(x)]; }
-	int find(int x) { return e[x] < 0 ? x : find(e[x]); }
-	int time() { return sz(st); }
-	void rollback(int t) {
-		for (int i = time(); i --> t;)
-			e[st[i].first] = st[i].second;
-		st.resize(t);
-	}
-	bool join(int a, int b) {
-		a = find(a), b = find(b);
-		if (a == b) return false;
-		if (e[a] > e[b]) swap(a, b);
-		st.push_back({a, e[a]});
-		st.push_back({b, e[b]});
-		e[a] += e[b]; e[b] = a;
-		return true;
-	}
-};
+ll Par[MXN], Sz[MXN]; vector<ll> hist;
+ll Find(ll x){ return (Par[x] == x ? x : Find(Par[x])); }
+bool Union(ll x, ll y){
+    x = Find(x), y = Find(y);
+    if(x == y) return 0;
+    if(Sz[x] < Sz[y]) swap(x, y);
+    Par[y] = x, Sz[x] += Sz[y];
+    hist.push_back(y);
+    return 1;
+}
+void Undo(){
+    ll x, y = hist.back();
+    x = Par[y], hist.pop_back();
+    Sz[x] -= Sz[y], Par[y] = y;
+}
+//iota(Par, Par + n + 1, 0);
+//fill(Sz, Sz + n + 1, 1);
